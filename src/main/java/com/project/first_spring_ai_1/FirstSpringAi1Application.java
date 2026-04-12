711fcae37afbba888bc4d9db37ff6e8f88b5d941
sanjana chowdary <sanjanachowdary@sanjanas-MacBook-Air.local>
2026-04-12 14:02:45 -0500

    first apring ai project files

---
+++ src/main/java/com/project/first_spring_ai_1/FirstSpringAi1Application.java
@@ -0,0 +1,13 @@
+package com.project.first_spring_ai_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/first_spring_ai_1/FirstSpringAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.first_spring_ai_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
